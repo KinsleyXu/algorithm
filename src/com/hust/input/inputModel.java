be66c4f4138a92ee3a91c311d77b90344b5cb205
xuxinyu06 <xuxinyu06@meituan.com>
2021-03-17 11:37:47 +0800

    leetcode1， test

---
+++ src/com/hust/input/inputModel.java
@@ -0,0 +1,19 @@
+package com.hust.input;
+
+import java.util.Scanner;
+
+public class inputModel {
+
+    /**
+     * 输入一个数组,例如2,7,11,15
+     */
+    public void inputArray(){
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next().toString();
+        String[] split = str.split(",");
+        int[] nums = new int[split.length];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(split[i]);
+        }
+    }
+}
